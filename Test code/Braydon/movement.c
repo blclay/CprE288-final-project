b6cb91f0362954c2e0b036a4f3f55ea5b01794d9
blclay <115607218+blclay@users.noreply.github.com>
2024-12-05 16:37:00 -0600

    Add files via upload

---
+++ Test code/Braydon/movement.c
@@ -0,0 +1,165 @@
+#include "open_interface.h"
+#include "Timer.h"
+//#include "cyBot_Scan.h"
+#include <stdio.h>
+#include <math.h>
+#include "movement.h"
+#include "uart_extra_help.h"
+//#include "adc.h"
+//#include "servo.h"
+//#include "ping.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "Timer.h"
+//#include "servo.h"
+//#include "lcd.h"
+//#include "button.h"
+#include <inc/tm4c123gh6pm.h>
+
+//volatile  char uart_data;  // Your UART interupt code can place read data here
+//volatile  char flag;       // Your UART interupt can update this flag
+
+typedef struct autostruct
+{
+    int minAngle;
+    float objDist;
+}autostruct_t;
+
+typedef struct{
+    float sound_dist;  // Distance from Ping Sensor (cyBOT_Scan returns -1.0 if PING is not enabled)
+    int IR_raw_val;    // Raw ADC value from IR sensor (cyBOT_Scan return -1 if IR is not enabled)
+} cyBOT_Scan_t;
+
+void move_forward_go_around(oi_t *sensor, int millimeters){
+
+     double sum = 0;
+     oi_setWheels(250, 250); // move forward; full speed
+
+     while (sum < millimeters) {
+     oi_update(sensor);
+     sum += sensor->distance;
+     if(sensor->bumpLeft){
+              move_backwards(sensor, 150);
+              sum -= 10;
+              turn_clockwise(sensor, 250);
+              move_forward(sensor, 250);
+              turn_cclockwise(sensor, 250);
+              oi_setWheels(250,250);
+              }
+     if(sensor->bumpRight){
+              move_backwards(sensor, 150);
+              sum -= 10;
+              turn_cclockwise(sensor, 250);
+              move_forward(sensor, 250);
+              turn_clockwise(sensor, 250);
+              oi_setWheels(250,250);
+              }
+
+     // running into a straight wall/hole
+     if((sensor->cliffFrontLeft) && (sensor->cliffFrontRight)){
+             move_backwards(sensor, 500);
+             sum -= 50;
+             turn_cclockwise(sensor, 180);
+             oi_setWheels(0, 0);
+             }
+
+     // 30 degrees into a corner
+     if((sensor->cliffFrontLeft) && (sensor->cliffLeft)){
+              move_backwards(sensor, 150);
+              sum -= 15;
+              turn_clockwise(sensor, 120);
+              }
+
+     //we are in the left corner (90 deg)
+     if(  ((sensor->cliffFrontLeft) && (sensor->cliffFrontRight)) && (sensor->cliffLeft)){
+         move_backwards(sensor, 150);
+         sum -= 15;
+         turn_clockwise(sensor, 90);
+         }
+
+     // we are in a right corner
+     if(((sensor->cliffFrontLeft) && (sensor->cliffFrontRight)) && (sensor->cliffLeft)){
+         move_backwards(sensor, 150);
+         sum -= 10;
+         turn_cclockwise(sensor, 300);
+         move_forward(sensor,100);
+         turn_clockwise(sensor, 250);
+         move_forward(sensor, 450);
+         turn_clockwise(sensor, 250);
+         oi_setWheels(150, 150);
+         }
+     if(sensor->cliffFrontRight){
+             move_backwards(sensor, 150);
+             sum -= 10;
+             turn_clockwise(sensor, 300);
+             move_forward(sensor, 100);
+             turn_cclockwise(sensor, 250);
+             move_forward(sensor, 450);
+             turn_cclockwise(sensor, 250);
+             oi_setWheels(150, 150);
+             }
+     }
+     oi_setWheels(0, 0); // stop
+}
+
+void move_forward(oi_t *sensor, int millimeters){
+
+     double sum = 0;
+     oi_setWheels(250, 250); // move forward; full speed
+
+     while (sum < millimeters) {
+     oi_update(sensor);
+     sum += sensor->distance;
+     if(sensor->bumpLeft){
+         move_backwards(sensor, 150);
+         sum -= 30;
+         turn_clockwise(sensor, 250);
+         move_forward(sensor, 250);
+         turn_cclockwise(sensor, 250);
+         oi_setWheels(250,250);
+         }
+     if(sensor->bumpRight){
+         move_backwards(sensor, 150);
+         sum -= 30;
+         turn_cclockwise(sensor, 250);
+         move_forward(sensor, 250);
+         turn_clockwise(sensor, 250);
+         oi_setWheels(250,250);
+         }
+     }
+     oi_setWheels(0, 0); // stop
+}
+void move_backwards(oi_t *sensor, int millimeters){
+
+     double sum = 0;
+     oi_setWheels(-250, -250); // move back; full speed
+
+     while (sum > -millimeters) {
+     oi_update(sensor);
+     sum += sensor->distance;
+     }
+     oi_setWheels(0, 0); // stop
+}
+
+void turn_clockwise(oi_t *sensor, int degrees){
+
+    double angle = 0;
+    oi_setWheels(-250, 250);
+    while (angle > -degrees) {
+    oi_update(sensor);
+    angle += (sensor->angle / 0.324056);
+    }
+    oi_setWheels(0, 0); // stop
+}
+void turn_cclockwise(oi_t *sensor, int degrees){
+
+    double angle = 0;
+    oi_setWheels(250, -250);
+    while (angle < degrees) {
+    oi_update(sensor);
+    angle += (sensor->angle / 0.324056);
+    }
+    oi_setWheels(0, 0); // stop
+}
+
+
